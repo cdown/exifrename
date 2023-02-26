835590e21d810287b63b2df627c643dd002fe754
Chris Down <chris@chrisdown.name>
2023-02-26 20:38:59 +0000

    main: Always show filename when a recoverable error occurs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use clap::Parser;
+use std::path::Path;
 
 use anyhow::Result;
+use clap::Parser;
 use hashbrown::HashMap;
 
 mod file;
@@ -9,26 +10,26 @@ mod metadata;
 mod types;
 mod util;
 
-fn main() -> Result<()> {
+fn handle_file(cfg: &types::Config, counter: &mut HashMap<String, u16>, file: &Path) -> Result<()> {
+    let new_name = format::get_new_name(cfg, file, counter)?;
+    println!("{} -> {}", file.display(), new_name);
+    if !cfg.dry_run {
+        if cfg.copy {
+            file::copy_creating_dirs(file, new_name, cfg.overwrite)?;
+        } else {
+            file::rename_creating_dirs(file, new_name, cfg.overwrite)?;
+        }
+    }
+    Ok(())
+}
+
+fn main() {
     let cfg = types::Config::parse();
     let mut counter: HashMap<String, u16> = HashMap::new();
 
     for file in &cfg.files {
-        match format::get_new_name(&cfg, file, &mut counter) {
-            Ok(new_name) => {
-                println!("{} -> {}", file.display(), new_name);
-                if !cfg.dry_run {
-                    if cfg.copy {
-                        file::copy_creating_dirs(file, new_name, cfg.overwrite)?;
-                    } else {
-                        file::rename_creating_dirs(file, new_name, cfg.overwrite)?;
-                    }
-                }
-            }
-
-            // Fatal conditions like invalid formats go through panic!(), not here
-            Err(err) => eprintln!("{}: {}", file.display(), err),
+        if let Err(err) = handle_file(&cfg, &mut counter, file) {
+            eprintln!("{}: {}", file.display(), err);
         }
     }
-    Ok(())
 }
