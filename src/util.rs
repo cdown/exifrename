e518b4adb10bb6ff80dc6de9c5a6fb2b17f9f1e1
Chris Down <chris@chrisdown.name>
2023-03-04 02:45:06 +0000

    util: get_usize_len can be a const function, mark it as such

--- src/util.rs
+++ src/util.rs
@@ -9,7 +9,7 @@ macro_rules! die {
 #[allow(unused_imports)]
 pub(crate) use die;
 
-pub fn get_usize_len(value_: usize) -> usize {
+pub const fn get_usize_len(value_: usize) -> usize {
     let mut len = 1;
     let mut value = value_;
     if value == 0 {
