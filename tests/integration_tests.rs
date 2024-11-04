edfed33ae5bab9fcfd8d74092ddef3a158acc850
Chris Down <chris@chrisdown.name>
2024-11-03 22:21:54 -0800

    tests: Check --dry-run works

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -70,3 +70,29 @@ fn test_rename_overwrite() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_dry_run() -> Result<()> {
+    let from_dir = tempfile::tempdir()?;
+    let from_path = from_dir.path();
+    let to_dir = tempfile::tempdir()?;
+    let to_path = to_dir.path();
+
+    fs::copy("tests/data/1.jpg", from_path.join("1.jpg"))?;
+
+    let mut cmd = assert_cmd::Command::cargo_bin("exifrename")?;
+    cmd.arg(from_path)
+        .arg("--dry-run")
+        .arg("--copy")
+        .arg("-f")
+        .arg("{year}_{month}_{day}")
+        .current_dir(to_path)
+        .assert()
+        .success();
+
+    // Check that the file was not copied
+    let expected_name = "2023_01_01.jpg";
+    assert!(!to_path.join(expected_name).exists());
+
+    Ok(())
+}
