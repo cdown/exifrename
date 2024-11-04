50e64cc93d2f4490503c071e75610d23d1f44b2d
Chris Down <chris@chrisdown.name>
2024-11-03 22:23:32 -0800

    tests: Add --no-counter tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -96,3 +96,51 @@ fn test_dry_run() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_no_counter() -> Result<()> {
+    let from_dir = tempfile::tempdir()?;
+    let from_path = from_dir.path();
+    let to_dir = tempfile::tempdir()?;
+    let to_path = to_dir.path();
+
+    fs::copy("tests/data/1.jpg", from_path.join("1.jpg"))?;
+    fs::copy("tests/data/1.jpg", from_path.join("2.jpg"))?;
+
+    let mut cmd = assert_cmd::Command::cargo_bin("exifrename")?;
+    cmd.arg(from_path)
+        .arg("--no-counter")
+        .arg("--copy")
+        .arg("-f")
+        .arg("{year}_{month}_{day}")
+        .current_dir(to_path)
+        .assert()
+        .failure();
+
+    Ok(())
+}
+
+#[test]
+fn test_no_counter_allow_overwrite() -> Result<()> {
+    // Test that with --no-counter, duplicate filenames cause an error
+    let from_dir = tempfile::tempdir()?;
+    let from_path = from_dir.path();
+    let to_dir = tempfile::tempdir()?;
+    let to_path = to_dir.path();
+
+    fs::copy("tests/data/1.jpg", from_path.join("1.jpg"))?;
+    fs::copy("tests/data/1.jpg", from_path.join("2.jpg"))?;
+
+    let mut cmd = assert_cmd::Command::cargo_bin("exifrename")?;
+    cmd.arg(from_path)
+        .arg("--overwrite")
+        .arg("--no-counter")
+        .arg("--copy")
+        .arg("-f")
+        .arg("{year}_{month}_{day}")
+        .current_dir(to_path)
+        .assert()
+        .success();
+
+    Ok(())
+}
